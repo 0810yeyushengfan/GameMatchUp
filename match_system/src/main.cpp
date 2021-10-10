6278198f84434956fac8be987421e8819cb02510
yeyushengfan <2160949409@qq.com>
2021-10-10 09:04:27 +0800

    match server:3.0

--- match_system/src/main.cpp
+++ match_system/src/main.cpp
@@ -17,6 +17,7 @@
 #include <condition_variable>
 #include <queue>
 #include <vector>
+#include <unistd.h>
 
 using namespace ::apache::thrift;
 using namespace ::apache::thrift::protocol;
@@ -72,10 +73,22 @@ class Pool
         {
             while(users.size()>1)
             {
-                auto a=users[0],b=users[1];
-                users.erase(users.begin());
-                users.erase(users.begin());
-                save_result(a.id,b.id);
+                sort(users.begin(),users.end(),[&](User& a,User b){
+                        return a.score<b.score;});
+                bool flag=true;
+                for(uint32_t i=1;i<users.size();i++)
+                {
+                    auto a=users[i-1],b=users[i];
+                    if(b.score-a.score<=50)
+                    {
+                        users.erase(users.begin()+i-1,users.begin()+i+1);
+                        save_result(a.id,b.id);
+                        flag=false;
+                        break;
+                    }
+                }
+                if(flag)
+                    break;
             }
         }
 
@@ -133,7 +146,10 @@ void consume_task()
         unique_lock<mutex> lck(message_queue.m);
         if(message_queue.q.empty())
         {
-            message_queue.cv.wait(lck);
+            //message_queue.cv.wait(lck);
+            lck.unlock();
+            pool.match();
+            sleep(1);
         }
         else
         {
